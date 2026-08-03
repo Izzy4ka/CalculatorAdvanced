0688f634fbeded011d31228c742355b45993fd01
Izzy4ka <baielaibek@mail.ru>
2026-08-03 09:40:27 +0600

    chore: update targetSdk versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.example.calculator"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
